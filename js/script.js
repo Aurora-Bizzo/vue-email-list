726211e690c271d8f901ef1b854c802fd155c4fe
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-01-09 22:35:40 +0100

    errore app

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue
+createApp ({
+    data(){
+        return {
+            arrayMail: [] 
+        }
+    },
+    methods: {
+
+        generateMails(){
+            for(let i=0; i<=10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.arrayMail.push(result.data.response);
+                console.log(this.arrayMail);
+                });
+            }
+        }
+        
+    },
+    
+}, ).mount('#app')
