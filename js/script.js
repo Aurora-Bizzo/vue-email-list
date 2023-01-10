4765b142fa51ee1ed47e0546cfab05cb6cca611c
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-01-10 21:55:11 +0100

    fixed and completed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
-const { createApp } = Vue
-createApp ({
+const { createApp } = Vue;
+
+createApp({
     data(){
         return {
             arrayMail: [] 
         }
     },
-    methods: {
+    mounted() {
 
-        generateMails(){
-            for(let i=0; i<=10; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                this.arrayMail.push(result.data.response);
-                console.log(this.arrayMail);
-                });
-            }
+        for(let i=0; i<=10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+            this.arrayMail.push(result.data.response)
+            })
         }
-        
-    },
-    
-}, ).mount('#app')
+    }  
+}).mount('#app');
